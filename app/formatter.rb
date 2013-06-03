c327b8b63d39e9ad0e7399f6fb82877c6483b449
Goffert van Gool <ruphin@ruphin.net>
2013-06-03 11:41:47 +0200

    Add gnuplot generator

--- app/formatter.rb
+++ app/formatter.rb
@@ -12,6 +12,38 @@ OptionParser.new do |opts|
   end
 end.parse!
 
+GNUPLOT_TEMPLATE = <<-eos
+#!/opt/local/bin/gnuplot
+# gnuplot 4.2 / Ubuntu 8.10 
+ 
+#input
+set datafile separator ";"
+ 
+#output
+set key top center
+set style data linespoints
+set grid
+ 
+set xlabel 'Step'
+set xrange [1:30]
+set xtics 5
+set mxtics 5
+ 
+set ylabel 'Measured Error'
+set ytics nomirror
+set ytics 5
+set yrange [10:50]
+ 
+set y2label 'Access Points Remaining'
+set y2tics 10
+set autoscale y2
+set y2range [0:100]
+set format y2 "%g%%"
+
+set term svg size 600,400 font "Arial,10"
+
+eos
+
 STEP = /INFO: STEP:(\d+)/
 ORIGINALAPS = /INFO: ORIGINAL-APS:(\d+)/
 APSET = /INFO: AP-SET:(.+)/
@@ -47,21 +79,38 @@ logfiles.each do |filename|
         dataset[step][:misses].push ["Learning " + data.captures.first, data.captures.last.to_f]
       end
     end
+
+    algorithms = dataset[0][:error].map(&:first)
+    original_aps = dataset[0][:originalaps].to_f
+    errors = dataset.map { |d| d[:error].map(&:last) }
+    errors = errors.zip( [Array.new(errors.first.size, 0)] + errors).map { |a, b| a.zip(b).map { |c, d| c - d }}
+    misses = dataset.map { |d| d[:misses].map(&:last) }
+    misses = misses.zip( [Array.new(misses.first.size, 0)] + misses).map { |a, b| a.zip(b).map { |c, d| c - d }}
+    average_error = errors.zip(misses).map { |a, b| a.zip(b).map {|c, d| c / (scans_per_iteration - d) }}
+
     File.open("#{filename}.data", 'w+') do |file|
-      algorithms = dataset[0][:error].map(&:first)
-      errors = dataset.map { |d| d[:error].map(&:last) }
-      errors = errors.zip( [Array.new(errors.first.size, 0)] + errors).map { |a, b| a.zip(b).map { |c, d| c - d }}
-      misses = dataset.map { |d| d[:misses].map(&:last) }
-      misses = misses.zip( [Array.new(misses.first.size, 0)] + misses).map { |a, b| a.zip(b).map { |c, d| c - d }}
-      average_error = errors.zip(misses).map { |a, b| a.zip(b).map {|c, d| c / (scans_per_iteration - d) }}
-
-
       file.puts "#{mapsize}\t#{density}\t#{accuracy}\t#{iterations}"
       file.puts ""
-      file.puts "step\toriginal-aps\t#{algorithms.join("\t")}\t#{algorithms.join("\t")}"
+      file.puts "step\taps-remaining\t#{algorithms.map {|a| a + " error"}.join("\t")}\t#{algorithms.map {|a| a + " misses"}.join("\t")}"
       dataset.each_with_index do |d, s|
-        file.puts "#{s+1}\t#{d[:originalaps]}\t#{average_error[s].join("\t")}\t#{misses[s].join("\t")}"
+        file.puts "#{s+1}\t#{d[:originalaps]/original_aps}\t#{average_error[s].join("\t")}\t#{misses[s].join("\t")}"
       end
+    end
+    File.open("#{filename}.gnuplot", 'w+') do |file|
+      file.puts GNUPLOT_TEMPLATE
+      file.puts "set output '#{mapsize}-#{density}-#{accuracy}-#{iterations}.svg'"
+
+      file.print "plot "
+      algorithms.each do |a|
+        file.puts "'-' using 1:($2) title '#{a}' axes x1y1 lt rgb 'black', \\"
+      end
+      file.puts "'-' using 1:($2*100) title 'Remaining Access Points' axes x1y2 lt rgb 'black'"
+      file.puts average_error.zip( Array.new(average_error.size) { |x| x+1 }).map { |a, b| a.map { |c| "#{b};#{c}" }}.transpose.map { |a| a.join("\n")}.join("\ne\n")
+      file.puts "e"
+      dataset.map { |e| e[:originalaps]/original_aps }.each_with_index do |d, s|
+        file.puts "#{s+1};#{d}"
+      end
+      file.puts "e"
     end
   end
 end
